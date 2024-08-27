84eb696ce6e95066a93ffc097e3a0f225679aefe
YuJeong0209 <iloveoyj0209@gmail.com>
2024-08-27 16:25:49 +0900

    chage createviews.py

--- fisa_django/blog/admin.py
+++ fisa_django/blog/admin.py
@@ -1,7 +1,8 @@
-#blog/admin.py
-
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment, Tag
+from django.contrib.auth.models import User
 
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Tag)
